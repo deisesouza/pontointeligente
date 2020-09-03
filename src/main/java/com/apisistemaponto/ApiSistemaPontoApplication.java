44ae1104aa2141b52651c2822673c6ee4a7395c5
Deise Souza <deise_santana_souza@hotmail.com>
2020-09-03 14:08:46 -0300

    atualizacao do projeto 2020

---
+++ src/main/java/com/apisistemaponto/ApiSistemaPontoApplication.java
@@ -0,0 +1,13 @@
+package com.apisistemaponto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSistemaPontoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiSistemaPontoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/apisistemaponto/ApiSistemaPontoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apisistemaponto;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSistemaPontoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
